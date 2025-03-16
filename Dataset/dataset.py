ad462631922c32e3d3529b10f4533a585a282e03
Joao Pedro Poloni Ponce <jpolonip@redhat.com>
2025-03-16 18:36:18 -0300

    :necktie:

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -1,17 +1,27 @@
 import os
 
 from torch.utils.data import Dataset
-from torchvision.io import read_image
+from torchvision.io import decode_image
 
 class LIDCDataset(Dataset):
     def __init__(self, img_dir, transform=None, target_transform=None):
-        self.input = []
-        self.mask = []
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
-        imgs = lis
+        raw = list(filter(lambda l: not l.endswith('edge_mask.png'), os.listdir(img_dir)))
+        self.input = list(filter(lambda l: not l.endswith('mask.png'), raw))
+        self.mask = list(filter(lambda l: l.endswith('mask.png'), raw))
+
+    def __len__(self):
+        return len(self.input)
 
     def __getitem__(self, idx):
-        image = self.input[idx]
-        mask = self.mask[idx]
+        image = decode_image(os.path.join(self.img_dir, self.input[idx]), mode='GRAY')
+        if self.transform:
+            image = self.transform(image)
+        mask = decode_image(os.path.join(self.img_dir, self.mask[idx]), mode='GRAY').div(255)
+        if self.target_transform:
+            mask = self.target_transform(mask)
 
         return [image, mask]
